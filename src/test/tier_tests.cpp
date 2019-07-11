9f718a406aa9cf7085f1f6e74cbb9fade285e53b
H-Pavel <>
2019-07-11 17:43:21 +0300

    Tier unit tests

--- src/test/tier_tests.cpp
+++ src/test/tier_tests.cpp
@@ -19,7 +19,7 @@ BOOST_AUTO_TEST_CASE(test_tier_from_outputs)
     CWallet wallet;
     int nextLockTime = 0;
     int nInput = 0;
-    unsigned int tiers[MasternodeTiers::TIER_NONE] = {MasternodeTiers::TIER_1K, MasternodeTiers::TIER_3K, MasternodeTiers::TIER_10K, MasternodeTiers::TIER_30K, MasternodeTiers::TIER_100K}
+    unsigned int tiers[MasternodeTiers::TIER_NONE] = {MasternodeTiers::TIER_1K, MasternodeTiers::TIER_3K, MasternodeTiers::TIER_10K, MasternodeTiers::TIER_30K, MasternodeTiers::TIER_100K};
 
     for (auto i = 0; i < MasternodeTiers::TIER_NONE; i++) {
         CMutableTransaction tx;
@@ -41,7 +41,7 @@ BOOST_AUTO_TEST_CASE(test_tier_from_outputs)
 
     CWalletTx wtxNotMn(&wallet, txNotMn);
 
-    BOOST_CHECK(IsMasternodeOutput(&wtxNotMn, nInput));
+    BOOST_CHECK(!IsMasternodeOutput(&wtxNotMn, nInput));
     BOOST_CHECK(!IsMasternodeOutput(&wtxNotMn, nInput + 2));
     BOOST_CHECK_EQUAL(GetMasternodeTierFromOutput(&wtxNotMn, nInput), MasternodeTiers::TIER_NONE);
 }
@@ -76,10 +76,58 @@ BOOST_AUTO_TEST_CASE(test_intial_distribution)
     }
 }
 
+BOOST_AUTO_TEST_CASE(test_single_tier)
+{
+    std::vector<int> vecBlockNumbers = {0, 1, 3, 4, 13, 14, 43, 44, 143, 144};
+    unsigned int tiers[MasternodeTiers::TIER_NONE] = {MasternodeTiers::TIER_1K, MasternodeTiers::TIER_3K, MasternodeTiers::TIER_10K, MasternodeTiers::TIER_30K, MasternodeTiers::TIER_100K};
+
+    vecTierSizes[0] = 1;
+    for (auto tier = 0; tier < MasternodeTiers::TIER_NONE; tier++) {
+        std::vector<size_t> vecTierSizes = {0, 0, 0, 0, 0};
+        vecTierSizes[tier] = 10;
+
+        for (auto i = 0; i < vecBlockNumbers.size(); i++) {
+            uint256 tmpHash(vecBlockNumbers[i]);
+            BOOST_CHECK_EQUAL(CalculateWinningTier(vecTierSizes, tmpHash), tiers[tier]);
+        }
+    }
+}
+
 BOOST_AUTO_TEST_CASE(test_winning_tier)
 {
+    std::vector<size_t> vecTierSizes1 = {100, 20, 10, 5, 2};
+    std::vector<int> vecBlockNumbers1 = {0, 23, 24, 37, 39, 61, 62, 96, 97, 143, 144};
+    std::vector<unsigned int> vecTiers1 = {MasternodeTiers::TIER_1K, MasternodeTiers::TIER_1K, MasternodeTiers::TIER_3K,
+                                           MasternodeTiers::TIER_3K, MasternodeTiers::TIER_10K, MasternodeTiers::TIER_10K,
+                                           MasternodeTiers::TIER_30K, MasternodeTiers::TIER_30K, MasternodeTiers::TIER_100K,
+                                           MasternodeTiers::TIER_100K, MasternodeTiers::TIER_1K};
+    for (auto j = 0; j < vecBlockNumbers1.size(); j++) {
+        uint256 tmpHash(vecBlockNumbers1[j]);
+        auto nTier = CalculateWinningTier(vecTierSizes1, tmpHash);
+        BOOST_CHECK_EQUAL(nTier, vecTiers1[j]);
+    }
 
+    std::vector<size_t> vecTierSizes2 = {100, 20, 10, 5, 0};
+    std::vector<int> vecBlockNumbers2 = {0, 10, 11, 16, 17, 27, 28, 43, 44};
+    std::vector<unsigned int> vecTiers2 = {MasternodeTiers::TIER_1K, MasternodeTiers::TIER_1K, MasternodeTiers::TIER_3K,
+                                           MasternodeTiers::TIER_3K, MasternodeTiers::TIER_10K, MasternodeTiers::TIER_10K,
+                                           MasternodeTiers::TIER_30K, MasternodeTiers::TIER_30K, MasternodeTiers::TIER_1K};
+    for (auto j = 0; j < vecBlockNumbers2.size(); j++) {
+        uint256 tmpHash(vecBlockNumbers2[j]);
+        auto nTier = CalculateWinningTier(vecTierSizes2, tmpHash);
+        BOOST_CHECK_EQUAL(nTier, vecTiers2[j]);
+    }
 
+    std::vector<size_t> vecTierSizes3 = {100, 40, 0, 5, 1};
+    std::vector<int> vecBlockNumbers3 = {0, 28, 29, 62, 63, 105, 106, 133, 134};
+    std::vector<unsigned int> vecTier3 = {MasternodeTiers::TIER_1K, MasternodeTiers::TIER_1K, MasternodeTiers::TIER_3K,
+                                           MasternodeTiers::TIER_3K, MasternodeTiers::TIER_30K, MasternodeTiers::TIER_30K,
+                                           MasternodeTiers::TIER_100K, MasternodeTiers::TIER_100K, MasternodeTiers::TIER_1K};
+    for (auto j = 0; j < vecBlockNumbers3.size(); j++) {
+        uint256 tmpHash(vecBlockNumbers3[j]);
+        auto nTier = CalculateWinningTier(vecTierSizes3, tmpHash);
+        BOOST_CHECK_EQUAL(nTier, vecTiers3[j]);
+    }
 }
 
 
